7a5e15946d82e76807328b969ea70823d2a00e4e
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2025-10-04 15:31:01 +0300

    primevue + frontpage setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import './assets/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+app.mount('#app')
