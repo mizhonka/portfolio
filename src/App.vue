05f7a660d7c810bb88c02f31168f3bc54a116df5
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2025-11-05 20:22:44 +0200

    animate on scroll

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <script setup lang="ts">
-import Menubar from 'primevue/menubar'
-import { ref } from 'vue'
-
-const menuItems = ref([
-  {
-    label: 'Home',
-  },
-  {
-    label: 'About',
-  },
-  {
-    label: 'Artwork',
-  },
-  {
-    label: 'Games',
-  },
-])
 </script>
 
 <template>
   <body>
-    <div class="navBar">
-      <Menubar :model="menuItems"></Menubar>
-    </div>
     <div class="tagline">
-      <h1>Miranda Honkanen</h1>
-      <h2>artist and game developer</h2>
+        <h1>Miranda Honkanen</h1>
+        <h2>artist and game developer</h2>
     </div>
+    <img src="./assets/icons/spider.png"></img>
+    <div class="h-[30rem]"></div>
+    <About v-animateonscroll="{ enterClass: 'animate-enter fade-in-10 slide-in-from-l-8 animate-duration-1000', leaveClass: 'animate-leave fade-out-0' }"></About>
   </body>
 </template>
 
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Skeleton from 'primevue/skeleton'
+</script>
+
+<template>
+  <body>
+    <Skeleton width="50rem" height="80rem"></Skeleton>
+  </body>
+</template>
+
+<style scoped></style>
